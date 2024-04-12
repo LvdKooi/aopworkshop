56a2d3fc492c9f51ff381d630e8e179bea602bba
Laurens van der Kooi <laurens.van.der.kooi@ordina.nl>
2024-04-12 11:10:46 +0200

    Initial commit

---
+++ src/main/java/nl/kooi/aopworkshop/AopWorkshopApplication.java
@@ -0,0 +1,13 @@
+package nl.kooi.aopworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/kooi/aopworkshop/AopWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.kooi.aopworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
